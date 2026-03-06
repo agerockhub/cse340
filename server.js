e2e702bcca3e044ddaf80be8cc7fd5335d7bae9a
agerockhub <agerock2013@gmail.com>
2026-03-06 07:22:44 +0000

    updated

--- server.js
+++ server.js
@@ -2,55 +2,30 @@ import express from 'express';
 import { fileURLToPath } from 'url';
 import path from 'path';
 
-// Define the the application environment
-const NODE_ENV = process.env.NODE_ENV?.toLowerCase() || 'production';
-
-// Define the port number the server will listen on
 const PORT = process.env.PORT || 3000;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-//FIXES FROM ME......
-import expressLayouts from "express-ejs-layouts";
 const app = express();
-app.use(expressLayouts);
-//MY FIXES END HERE....
 
-// Set EJS as the templating engine
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'src', 'views'));
 
-// Tell Express where to find your templates
-app.set('views', path.join(__dirname, 'src/views'));
-
-/**
-  * Configure Express middleware
-  */
-
-// Serve static files from the public directory
 app.use(express.static(path.join(__dirname, 'public')));
 
-/**
-
-/**
- * Routes
- */
-app.get('/', async (req, res) => {
-    const title = 'Home';
-    res.render('home', { title });
+app.get('/', (req, res) => {
+  res.render('home', { title: 'Home' });
 });
 
-app.get('/organizations', async (req, res) => {
-    const title = 'Our Partner Organizations';
-    res.render('organizations', { title });
+app.get('/organizations', (req, res) => {
+  res.render('organizations', { title: 'Our Partner Organizations' });
 });
 
-app.get('/projects', async (req, res) => {
-    const title = 'Service Projects';
-    res.render('projects', { title });
+app.get('/projects', (req, res) => {
+  res.render('projects', { title: 'Service Projects' });
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running at http://127.0.0.1:${PORT}`);
-  console.log(`Environment: ${NODE_ENV}`);
+  console.log(`Server running on port ${PORT}`);
 });
